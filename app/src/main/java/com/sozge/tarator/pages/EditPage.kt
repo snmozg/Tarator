73fbf650a85738c181271b83fc256d3e1b1422e2
sozge <sozgeozkartal@gmail.com>
2024-10-30 15:31:06 +0300

    BottomSheet

--- app/src/main/java/com/sozge/tarator/pages/EditPage.kt
+++ app/src/main/java/com/sozge/tarator/pages/EditPage.kt
@@ -18,28 +18,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Adb
 import androidx.compose.material.icons.rounded.AddCircleOutline
 import androidx.compose.material.icons.rounded.Brush
 import androidx.compose.material.icons.rounded.Crop
 import androidx.compose.material.icons.rounded.FilterAlt
 import androidx.compose.material.icons.rounded.SaveAlt
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
@@ -48,9 +54,8 @@ import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.sozge.tarator.CustomButton
 import com.sozge.tarator.bars.AppBar
-import com.sozge.tarator.ui.theme.TaratorTheme
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditPageScreen(navController: NavController) {
     var hasPermission by remember { mutableStateOf(false) }
@@ -141,7 +146,34 @@ fun EditPageScreen(navController: NavController) {
                 )
 
                 Spacer(modifier = Modifier.height(50.dp))
-
+                val sheetState = rememberModalBottomSheetState()
+                var isSheetOpen by rememberSaveable {
+                    mutableStateOf(false)
+                }
+                if(isSheetOpen) {
+                    ModalBottomSheet(
+                        sheetState = sheetState,
+                        onDismissRequest = {
+                            isSheetOpen = false },
+                    ) {
+                       LazyRow() {
+                           items(1){
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                               Icon(imageVector = Icons.Rounded.Adb, contentDescription = "")
+                           }
+                       }
+                    }
+                }
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -153,8 +185,7 @@ fun EditPageScreen(navController: NavController) {
                         Icons.Rounded.FilterAlt,
                         "Filter Button",
                         "FILTERS",
-                        onClick = {
-                        }
+                        onClick = { isSheetOpen=true}
                     )
                     CustomButton(
                         Icons.Rounded.Crop,
